0c8e1c93bd9618d0ff3176ac83aaaf4fef0499be
Noor Ahmad Naufal <nohanbluu@gmail.com>
2026-04-02 14:50:37 +0800

    initial project setup - Modul Praktikum 1

---
+++ scripts/test_mongo.py
@@ -0,0 +1,12 @@
+﻿from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+# GANTI URI DI BAWAH DENGAN CONNECTION STRING MONGODB ATLAS KAMU
+uri = "mongodb+srv://USERNAME:PASSWORD@cluster0.xxxxx.mongodb.net/?appName=Cluster0"
+
+client = MongoClient(uri, server_api=ServerApi('1'))
+try:
+    client.admin.command('ping')
+    print("BERHASIL terhubung ke MongoDB Atlas!")
+except Exception as e:
+    print(f"GAGAL: {e}")
